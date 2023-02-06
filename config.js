4db2fa846cd47e5001f22ab3b50583f66f99528c
Trien1402 <88195796+Trien1402@users.noreply.github.com>
2023-02-06 16:59:24 +0300

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/miusilly?mibextid=LQQJ4d';
+const eventName = 'Chúc Mừng Valentine 14-02';
+const titleCard = 'Tặng cục ương';
+const contentCard = ' Chúc mừng ngày lễ e bé của anh. Chúc e luôn xinh đẹp , luôn vui vẻ và nhanh giàu . Hehe nhớ em 😍';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
